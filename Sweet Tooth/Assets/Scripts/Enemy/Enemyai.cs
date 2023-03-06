3e1304fb197c6867b1776ba1d1b0f28ef47aa331
Bbest930 <60197220+Bbest930@users.noreply.github.com>
2023-03-06 11:51:38 -0500

    edited ai and added in crazycorn model

--- Sweet Tooth/Assets/Scripts/Enemy/Enemyai.cs
+++ Sweet Tooth/Assets/Scripts/Enemy/Enemyai.cs
@@ -1,62 +1,53 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class EnemyAi : MonoBehaviour
 {
-    public NavMeshAgent agent;
-    public Transform player;
-    public LayerMask whatIsGround, whatIsPlayer;
+    NavMeshAgent agent;
+    Transform player;
+    public float sightRange = 10f;
 
-    // patrolling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
 
-    // attacking
-    public float timeAttack;
-    bool alreadyAttacked;
-
+   
     // states
-    public float attackRange, sightRange;
+    public float attackRange;
     bool playerInAttackRange, playerInSightRange;
+    
 
-    private void Awake() {
-        player = GameObject.Find("PlayerObj").transform;
+    void Start() {
+
+        //target = player
         agent = GetComponent<NavMeshAgent>();
     }
 
-    private void Update() {
+
+    void Update() {
         // checks for player in sight range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+
+
+
+
+
+
+
        
-       if (!playerInSightRange && !playerInAttackRange) Patrolling();
        if (playerInSightRange && !playerInAttackRange) chasePlayer();
-       if (playerInSightRange && playerInAttackRange) attackPlayer();
+       
     }
 
-    private void Patrolling() {
 
-    }
 
-    private void chasePlayer() {
+
+    void chasePlayer() {
         // approaches player position
-        agent.SetDestination(player.position);
+        agent.SetDestination(player.transform.position);
+
 
     }
 
-    private void attackPlayer() {
-        // stops enemy when attacking
 
-        // agent.setDestination(transform.position);
-        // transform.lookAt(player);
-
-        if (!alreadyAttacked) {
-            alreadyAttacked = true; 
-        }
-    }
-
-    private void alreadyAttackedPlayer() {
-
-    }
+   
 }
+
