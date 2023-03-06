5bf6c4e4e91f7b3b213cefe6af2dd3a4f3e68b42
tylersfoot <tylerdepa8@gmail.com>
2023-03-05 19:35:39 -0500

    actually fixed gumball collision, added spread

--- Sweet Tooth/Assets/Scripts/Tools/BubblegumBlaster.cs
+++ Sweet Tooth/Assets/Scripts/Tools/BubblegumBlaster.cs
@@ -11,11 +11,22 @@ public class BubblegumBlaster : MonoBehaviour
     public float fireRate; // fire rate of projectile
     public float range; // how far the projectile travels
     public float lifespan; // how long until the projectile despawns
+    public float spread; // max spread angle deviation in degrees
     private float fireCooldown = 0f; // time until next shot
     public bool isKeyDown = false;
 
     // array of colors for the gumball
     public Color[] colors = new Color[] {Color.red, Color.green, Color.blue, Color.yellow};
+
+    void Start()
+    {
+        // ! blaster does not have collider so not necessary (if it does in the future uncomment this)
+        // get the colliders for the player and gumball
+        // Collider playerCollider = GameObject.FindWithTag("Player").GetComponent<Collider>();
+        // Collider gumballCollider = projectilePrefab.GetComponent<Collider>();
+        // ignore collisions between the player and gumball
+        // Physics.IgnoreCollision(playerCollider, gumballCollider);
+    }
 
     void Update()
     {
@@ -29,11 +40,18 @@ public class BubblegumBlaster : MonoBehaviour
 
     public void Shoot()
     {
+        // calculate random spread angles
+        float spreadAngleX = Random.Range(-spread, spread);
+        float spreadAngleY = Random.Range(-spread, spread);
+
+        // apply the spread angles to the projectile's forward direction
+        Vector3 spreadVector = Quaternion.Euler(spreadAngleX, spreadAngleY, 0f) * projectileSpawn.forward;
+    
         // spawn a new projectile at the shoot point
         GameObject newProjectile = Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation, GameObject.Find("Projectiles").transform);
-        // apply a force to the projectile in the shoot point's forward direction
+        // apply a force to the projectile in the shoot point's forward direction with randomized spread
         Rigidbody projectileRb = newProjectile.GetComponent<Rigidbody>();
-        projectileRb.AddForce(projectileSpawn.forward * shootForce, ForceMode.Impulse);
+        projectileRb.AddForce(spreadVector * shootForce, ForceMode.Impulse);
 
         // get the Renderer component of the new projectile
         Renderer projectileRenderer = newProjectile.GetComponent<Renderer>();
@@ -42,7 +60,7 @@ public class BubblegumBlaster : MonoBehaviour
         // set the color of the projectile to the random color
         projectileRenderer.material.color = randomColor;
 
-        // destroy the projectile after 5 seconds
+        // destroy the projectile after the specified lifespan
         StartCoroutine(DestroyAfterTime(newProjectile, lifespan));
     }
 
