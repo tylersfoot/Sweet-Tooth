86a2eb9fab65e58ea355688aff6ea66beeaf49d1
tylersfoot <tylerdepa8@gmail.com>
2023-03-06 13:38:41 -0500

    imported crazycorn model w/ physics, cleaned up Enemyai.cs

--- Sweet Tooth/Assets/Scripts/Enemy/Enemyai.cs
+++ Sweet Tooth/Assets/Scripts/Enemy/Enemyai.cs
@@ -1,53 +1,33 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-
 public class EnemyAi : MonoBehaviour
 {
     NavMeshAgent agent;
     Transform player;
     public float sightRange = 10f;
-
-
    
     // states
     public float attackRange;
-    bool playerInAttackRange, playerInSightRange;
-    
+    public bool playerInAttackRange;
+    public bool playerInSightRange;
 
     void Start() {
-
-        //target = player
+        // target = player
         agent = GetComponent<NavMeshAgent>();
     }
 
-
     void Update() {
-        // checks for player in sight range
-
-
-
-
-
-
-
-
-       
-       if (playerInSightRange && !playerInAttackRange) chasePlayer();
-       
+    // checks for player in sight range
+       if (playerInSightRange && !playerInAttackRange)
+       {
+        chasePlayer();
+       }
     }
-
-
-
 
     void chasePlayer() {
         // approaches player position
         agent.SetDestination(player.transform.position);
-
-
     }
-
-
-   
 }
 
