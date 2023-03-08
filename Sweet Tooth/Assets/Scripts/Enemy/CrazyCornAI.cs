d42d049628c3635f718648ef0aea769e741736f6
Bbest930 <60197220+Bbest930@users.noreply.github.com>
2023-03-08 12:14:38 -0500

    copied code for gummy bear and added in patrol and attack functions

--- Sweet Tooth/Assets/Scripts/Enemy/CrazyCornAI.cs
+++ Sweet Tooth/Assets/Scripts/Enemy/CrazyCornAI.cs
@@ -5,12 +5,17 @@ public class CrazyCornAI : MonoBehaviour
 {
     NavMeshAgent agent;
     Transform target;
-    public float sightRange; // distance the enemy can see the player
+    public float sightRange = 10f; // distance the enemy can see the player
+    public float attackRange = 1f; // distance the enemy can attack the player
+    public Transform[] moveSpots;
+    private int randomSpot;
+    public float speed;
 
     void Start() {
         // target = player
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        randomSpot = Random.Range(0, moveSpots.Length);
     }
 
     void Update() {
@@ -20,11 +25,30 @@ public class CrazyCornAI : MonoBehaviour
         {
             chasePlayer();
         }
+        if (distance <= attackRange) 
+        {
+            attackPlayer();
+        }
+        if (distance >= 10f)
+        {
+            patrol();
+        }
     }
 
     void chasePlayer() {
         // approaches player position
         agent.SetDestination(target.transform.position);
+    }
+
+    void attackPlayer() {
+        //stops approaching
+        agent.SetDestination(transform.position);
+
+        //I need the code for the damage here and animation
+    }
+    void patrol() {
+        //
+        agent.transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
     }
 }
 
