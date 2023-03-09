9afce971c91253d1ebd7396abc9be11c75edd427
Bbest930 <60197220+Bbest930@users.noreply.github.com>
2023-03-09 12:15:27 -0500

    finsihed the patrol feature and made new tool model

--- Sweet Tooth/Assets/Scripts/Enemy/CrazyCornAI.cs
+++ Sweet Tooth/Assets/Scripts/Enemy/CrazyCornAI.cs
@@ -7,15 +7,16 @@ public class CrazyCornAI : MonoBehaviour
     Transform target;
     public float sightRange = 10f; // distance the enemy can see the player
     public float attackRange = 1f; // distance the enemy can attack the player
-    public Transform[] moveSpots;
-    private int randomSpot;
-    public float speed;
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    public LayerMask Terrain;
 
     void Start() {
         // target = player
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        randomSpot = Random.Range(0, moveSpots.Length);
+       
     }
 
     void Update() {
@@ -47,8 +48,40 @@ public class CrazyCornAI : MonoBehaviour
         // I need the code for the damage here and animation
     }
     void patrol() {
-        // ! this throws an error every frame so I commented it out lol
-        // agent.transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        
+        if (!walkPointSet) {
+
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet) {
+
+        agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        //Walkpoint arrived
+
+        if (distanceToWalkPoint.magnitude < 1f) {
+
+            walkPointSet = false;
+
+        }
+
+    }
+
+    void SearchWalkPoint() {
+
+        //calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3 (transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, Terrain)) {
+        walkPointSet = true;
+        }
+
     }
 }
 
---
+++ Sweet Tooth/Assets/Scripts/Enemy/GummyBearAI.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GummyBearAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Transform target;
+    public float sightRange = 10f; // distance the enemy can see the player
+    public float attackRange = 1f; // distance the enemy can attack the player
+    public Transform[] moveSpots;
+    private int randomSpot;
+    public float speed;
+
+    void Start() {
+        // target = player
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    void Update() {
+        float distance = Vector3.Distance(target.position, transform.position);
+        // checks for player in sight range
+        if (distance <= sightRange)
+        {
+            chasePlayer();
+        }
+        if (distance <= attackRange) 
+        {
+            attackPlayer();
+        }
+        if (distance >= 10f)
+        {
+            patrol();
+        }
+    }
+
+    void chasePlayer() {
+        // approaches player position
+        agent.SetDestination(target.transform.position);
+    }
+
+    void attackPlayer() {
+        // stops approaching
+        agent.SetDestination(transform.position);
+
+        // I need the code for the damage here and animation
+    }
+    void patrol() {
+        // ! this throws an error every frame so I commented it out lol
+        // agent.transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+    }
+}
+
